2ee546d93f2f71777417352bb91ae1b1c1318957
Erin Platvoet <erin.didomenico520@gmail.com>
2020-07-31 09:57:35 -0400

    rows and columns set in HTML for now

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,7 @@ var dateTime = new Date ();
 $("#currentDay").text(dateTime);
 console.log(new Date());
 
+// function for inputting tasks under each hour
+// lock button & function for each hour
+
+//if/else statements regarding time to change the color on screen (style already in css)
