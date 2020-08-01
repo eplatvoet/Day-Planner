61b27f0e7084478162e494e98ede245590084527
Erin Platvoet <erin.didomenico520@gmail.com>
2020-08-01 10:08:30 -0400

    displayed time with it refreshing constantly

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,25 @@ $(document).ready(function () {
 });
 console.log("I'm ready!");
 
+//global variables set up
+var saveBtn = $(".saveBtn")
+
+//date & time displayed
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+//function to update & display date and time
+function updateClock() {
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+}
+
+setInterval(updateClock, 1000)
+
 
 //for loop created to set up hourly rows
 for (var hour = 9; hour < 18; hour++) {
     var hourlyRow = $("<div>");
-    hourlyRow.attr("value", [hour]);
     hourlyRow.addClass("row");
+    hourlyRow.attr("value", [hour]);
 
     //creating the first column of each row
     var colOne = $("<div>");
@@ -29,7 +42,7 @@ for (var hour = 9; hour < 18; hour++) {
     var colTwo = $("<textarea>");
     colTwo.addClass("col-md-8");
     colTwo.attr("value", [hour]);
-    colTwo.addClass("text-area");
+    colTwo.addClass("text-area userTasks");
     colTwo.attr("placeholder", "Enter your task(s) here")
     
     // creates button for save and third col
@@ -47,77 +60,42 @@ for (var hour = 9; hour < 18; hour++) {
 
 }
 
+//variables set up to access columns
+var userTasks = $("userTasks")
 
-// // lock button & function for each hour
-// saveBtn.on("click", function saveTask() {
-//     var task = userTasks.value.trim();
-//     if (userTasks !== "") {
-//         var savedTask = JSON.parse(localStorage.getItem("savedTask")) || []
-//         var taskObject = {
-//             task: task,
-//             entered: entered
-//         }
-//         console.log(scoreObject);
-//         savedTask.push(scoreObject);
-//         console.log(savedTask);
-//         localStorage.setItem("savedTask", JSON.stringify(savedTask));
-//     }
-// });
-// submitInitials.addEventListener("click", function () {
-//     event.preventDefault();
-//     userTasks.value = "";
-//     saveTask();
-//     showTasks();
-// });
+// lock button & function for each hour
+// saving tasks to local storage
+function saveTask() {
+    var task = userTasks.value.trim();
+    if (userTasks !== "") {
+        var savedTask = JSON.parse(localStorage.getItem("savedTask")) || [];
+        var taskObject = {
+            hour: hour,
+            task: task
+        };
+        console.log(taskObject);
+        savedTask.push(taskObject);
+        console.log(savedTask);
+        localStorage.setItem("savedTask", JSON.stringify(savedTask));
+    }
+};
+saveBtn.addEventListener("click", function () {
+    event.preventDefault();
+    userTasks.value = "";
+    saveTask();
+    showTasks();
+});
 
-// function showTasks() {
-//     var savedTask = JSON.parse(localStorage.getItem("savedTask")) || []
-//     savedTask.forEach(function(score) {
-//         var textAreas = $("textarea");
-//         textAreas.textContent = userTasks
-//         var lsTasks = document.getElementById("user-initials");
+function showTasks() {
+    var savedTask = JSON.parse(localStorage.getItem("savedTask")) || []
+    savedTask.forEach(function(score) {
+        var textAreas = $(".text-area");
+        textAreas.textContent = userTasks.hour, userTasks.task
+        var lsTasks = document.getElementById("user-initials");
 
-//         lsTasks.appendChild(textAreas);
-//     })
-// };
+        lsTasks.appendChild(textAreas);
+    })
+};
 
 //if/else statements regarding time to change the color on screen (style already in css)
-
-
-
-// saving tasks to local storage
-
-
-
-// function saveScore() {
-//     var initials = initialsElement.value.trim();
-//     if (initials !== "") {
-//         var highScore = JSON.parse(localStorage.getItem("highScore")) || []
-//         var scoreObject = {
-//             score: score,
-//             initials: initials
-//         }
-//         console.log(scoreObject);
-//         highScore.push(scoreObject);
-//         console.log(highScore);
-//         localStorage.setItem("highScore", JSON.stringify(highScore));
-//     }
-// }
-
-
-// submitInitials.addEventListener("click", function () {
-//     event.preventDefault();
-//     initialsElement.value = "";
-//     saveScore();
-//     showScore();
-// });
-
-// function showScore() {
-//     var highScore = JSON.parse(localStorage.getItem("highScore")) || []
-//     highScore.forEach(function(score) {
-//         var liTags = document.createElement("li");
-//         liTags.textContent = score.initials + " - " + score.score;
-//         var scoreElement = document.getElementById("user-initials");
-
-//         scoreElement.appendChild(liTags);
-//     })
+// if ()
